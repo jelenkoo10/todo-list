aebc16d645ede62caf5717588a3a100b9b8097f8
jelenkoo10 <veljkojelenkovic00@gmail.com>
2023-10-23 00:04:53 +0200

    Splitting todos to days

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,27 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
+const select = document.getElementById("day");
+const errorText = document.getElementById("error-text");
 const form = document.querySelector("form");
 const todolist = document.querySelector("ul");
+const currentDay = new Date().getDay();
+select.value = currentDay.toString();
+let selectedDay = currentDay;
+select.addEventListener("change", function () {
+    selectedDay = parseInt(select.value);
+    todolist.innerHTML = "";
+    todos = readTodos();
+    todos.forEach((todo) => createTodo(todo));
+});
 const readTodos = () => {
-    const todosJSON = localStorage.getItem("todos");
+    const todosJSON = localStorage.getItem(selectedDay === null || selectedDay === void 0 ? void 0 : selectedDay.toString());
     if (todosJSON === null)
         return [];
     return JSON.parse(todosJSON);
 };
 const saveTodos = () => {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem(selectedDay.toString(), JSON.stringify(todos));
 };
 const createTodo = (todo) => {
     const newLi = document.createElement("li");
@@ -26,10 +37,14 @@ const createTodo = (todo) => {
     newLi.append(checkbox);
     todolist.append(newLi);
 };
-const todos = readTodos();
+let todos = readTodos();
 todos.forEach((todo) => createTodo(todo));
 const handleSubmit = (e) => {
     e.preventDefault();
+    if (input.value === "") {
+        errorText.textContent = "Todo cannot be empty!";
+        return null;
+    }
     const newTodo = {
         text: input.value,
         completed: false,
@@ -38,5 +53,6 @@ const handleSubmit = (e) => {
     todos.push(newTodo);
     saveTodos();
     input.value = "";
+    errorText.textContent = "";
 };
 form.addEventListener("submit", handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,39 @@ interface Todo {
   completed: boolean;
 }
 
+interface DayPlan {
+  day: string;
+  list: Todo[];
+}
+
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const select = document.getElementById("day")! as HTMLSelectElement;
+const errorText = document.getElementById(
+  "error-text"
+)! as HTMLParagraphElement;
 const form = document.querySelector("form")!;
 const todolist = document.querySelector("ul")!;
 
+const currentDay: number = new Date().getDay();
+select.value = currentDay.toString();
+let selectedDay: number = currentDay;
+
+select.addEventListener("change", function () {
+  selectedDay = parseInt(select.value);
+  todolist.innerHTML = "";
+  todos = readTodos();
+  todos.forEach((todo) => createTodo(todo));
+});
+
 const readTodos = (): Todo[] => {
-  const todosJSON = localStorage.getItem("todos");
+  const todosJSON = localStorage.getItem(selectedDay?.toString());
   if (todosJSON === null) return [];
   return JSON.parse(todosJSON);
 };
 
 const saveTodos = () => {
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem(selectedDay.toString(), JSON.stringify(todos));
 };
 
 const createTodo = (todo: Todo) => {
@@ -33,12 +53,17 @@ const createTodo = (todo: Todo) => {
   todolist.append(newLi);
 };
 
-const todos: Todo[] = readTodos();
+let todos: Todo[] = readTodos();
 
 todos.forEach((todo) => createTodo(todo));
 
 const handleSubmit = (e: SubmitEvent) => {
   e.preventDefault();
+  if (input.value === "") {
+    errorText.textContent = "Todo cannot be empty!";
+    return null;
+  }
+
   const newTodo: Todo = {
     text: input.value,
     completed: false,
@@ -48,6 +73,7 @@ const handleSubmit = (e: SubmitEvent) => {
 
   saveTodos();
   input.value = "";
+  errorText.textContent = "";
 };
 
 form.addEventListener("submit", handleSubmit);
